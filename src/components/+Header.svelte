0ec87b69c9cecff72a0c470ecaae2f7466e4ef7c
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2023-11-20 12:36:38 +0200

    refactor and setup file tree

---
+++ src/components/+Header.svelte
@@ -0,0 +1,6 @@
+<div class="container d-flex p-2 align-items-center mb-3 bg-white shadow-sm">
+    <img src="logo.png" width="30" height="30" alt="" />
+    <h1 style="color: #338a93;" class="ms-2 display-4 fw-semibold mb-0">
+        WorkShop
+    </h1>
+</div>
---
+++ src/components/+Navigation.svelte
@@ -0,0 +1,14 @@
+<section class="row text-center py-2 shadow">
+    <a href="" class="col text-decoration-none" style="color: #338a93;">
+        <i class="bi bi-grid-fill" />
+        <p class="mb-0 small">microapps</p>
+    </a>
+    <a href="" class="col text-decoration-none" style="color: #338a93;">
+        <i class="bi bi-journal-text" />
+        <p class="mb-0 small">Guides</p>
+    </a>
+    <a href="" class="col text-decoration-none" style="color: #338a93;">
+        <i class="bi bi-person-fill" />
+        <p class="mb-0 small">Profile</p>
+    </a>
+</section>
